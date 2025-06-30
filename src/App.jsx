df45ce5b3686e64af7363ee4c6e83edf638ae0ed
monubawane2861 <monubawane2861>
2025-06-30 14:49:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
 import Contact from "./components/pages/Contact/Contact";
-import Services from "./components/pages/service/Services";
+import Services from "./components/pages/Services/Services";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
